2dedd197a0f1545fdb9b21d60ec793b30176d305
philippe.marschall <philippe.marschall@5ce95a82-6847-0410-8036-a182123a760a>
2010-06-05 12:52:55 +0000

    - bump

---
+++ trunk/pharo-build/src/main/java/st/seaside/PharoBuilder.java
@@ -0,0 +1,140 @@
+package st.seaside;
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.util.FormValidation;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.AbstractProject;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.QueryParameter;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * {@link Builder} for <a href="http://www.pharo-project.org/">Pharo</a>
+ * images.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link PharoBuilder} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(Build, Launcher, BuildListener)} method
+ * will be invoked. 
+ *
+ * @author Philippe Marschall
+ */
+public class PharoBuilder extends Builder {
+
+    private final String name;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public PharoBuilder(String name) {
+        this.name = name;
+    }
+
+    /**
+     * We'll use this from the <tt>config.jelly</tt>.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+        // this is where you 'build' the project
+        // since this is a dummy, we just say 'hello world' and call that a build
+
+        // this also shows how you can consult the global configuration of the builder
+        if(getDescriptor().useFrench())
+            listener.getLogger().println("Bonjour, " + name + "!");
+        else
+            listener.getLogger().println("Hello, " + name + "!");
+        return true;
+    }
+
+    // overrided for better type safety.
+    // if your plugin doesn't really define any property on Descriptor,
+    // you don't have to do this.
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl) super.getDescriptor();
+    }
+
+    /**
+     * Descriptor for {@link PharoBuilder}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     *
+     * <p>
+     * See <tt>views/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * for the actual HTML fragment for the configuration screen.
+     */
+    @Extension // this marker indicates Hudson that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        /**
+         * To persist global configuration information,
+         * simply store it in a field and call save().
+         *
+         * <p>
+         * If you don't want fields to be persisted, use <tt>transient</tt>.
+         */
+        private boolean useFrench;
+
+        /**
+         * Performs on-the-fly validation of the form field 'name'.
+         *
+         * @param value
+         *      This parameter receives the value that the user has typed.
+         * @return
+         *      Indicates the outcome of the validation. This is sent to the browser.
+         */
+        public FormValidation doCheckName(@QueryParameter String value) throws IOException, ServletException {
+            if(value.isEmpty())
+                return FormValidation.error("Please set a name");
+            if(value.length() < 4)
+                return FormValidation.warning("Isn't the name too short?");
+            return FormValidation.ok();
+        }
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // indicates that this builder can be used with all kinds of project types 
+            return true;
+        }
+
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Pharo Image Builder";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            // To persist global configuration information,
+            // set that to properties and call save().
+            useFrench = formData.getBoolean("useFrench");
+            // ^Can also use req.bindJSON(this, formData);
+            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
+            save();
+            return super.configure(req,formData);
+        }
+
+        /**
+         * This method returns true if the global configuration says we should speak French.
+         */
+        public boolean useFrench() {
+            return useFrench;
+        }
+    }
+}
+
