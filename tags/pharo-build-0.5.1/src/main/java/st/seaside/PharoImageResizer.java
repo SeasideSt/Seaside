d86c0f03b362a7b766950a13fecf76f8f348b1b2
philippe.marschall <philippe.marschall@5ce95a82-6847-0410-8036-a182123a760a>
2010-07-26 05:09:06 +0000

    [maven-release-plugin]  copy for tag pharo-build-0.5.1

---
+++ tags/pharo-build-0.5.1/src/main/java/st/seaside/PharoImageResizer.java
@@ -0,0 +1,119 @@
+package st.seaside;
+
+import java.io.IOException;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import st.seaside.PharoImageResizer.DescriptorImpl;
+
+/**
+ * {@link Builder} for <a href="http://www.pharo-project.org/">Pharo</a>
+ * images.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link PharoImageResizer} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)} method
+ * will be invoked.
+ *
+ * @author Philippe Marschall
+ */
+public class PharoImageResizer extends Builder {
+
+  private final String image;
+  private final int width;
+  private final int height;
+
+  @DataBoundConstructor
+  public PharoImageResizer(String image, int width, int height) {
+    this.image = image;
+    this.width = width;
+    this.height = height;
+  }
+
+  public String getImage() {
+    // needed by Jelly, don't remove
+    return this.image;
+  }
+
+  public int getWidth() {
+    // needed by Jelly, don't remove
+    return this.width;
+  }
+
+  public int getHeight() {
+    // needed by Jelly, don't remove
+    return this.height;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+      throws InterruptedException, IOException {
+    listener.getLogger().println("Not yet implemented");
+    return false;
+  }
+
+
+  /**
+   * Descriptor for {@link PharoImageResizer}. Used as a singleton.
+   * The class is marked as public so that it can be accessed from views.
+   *
+   * <p>
+   * See <tt>views/hudson/plugins/pharo-build/OneClickBuilder/*.jelly</tt>
+   * for the actual HTML fragment for the configuration screen.
+   */
+  public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+
+    /**
+     * Default constructor, loads the defaults first and then the saved data.
+     */
+    public DescriptorImpl() {
+      super(PharoImageResizer.class);
+      load();
+    }
+
+    /**
+     * Constructor, only loads the defaults and not the saved data.
+     *
+     * @param clazz the builder class
+     */
+    protected DescriptorImpl(Class<? extends PharoBuilder> clazz) {
+      super(clazz);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+      return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDisplayName() {
+      return Messages.imageResizer_displayName();
+    }
+
+  }
+
+}
